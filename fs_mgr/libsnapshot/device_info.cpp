fc15bf1352d411efc3c52f3928afcfd6c60da13d
bigbiff <bigbiff@teamw.in>
2021-06-20 16:21:48 -0400

    recovery: unconditionally specify recovery mode in twrp
    Change-Id: I2566f4c55c7e626ccc01bd817b1bf620b0e8605b

--- fs_mgr/libsnapshot/device_info.cpp
+++ fs_mgr/libsnapshot/device_info.cpp
@@ -32,11 +32,7 @@ using android::hal::CommandResult;
 using namespace std::chrono_literals;
 using namespace std::string_literals;
 
-#ifdef __ANDROID_RECOVERY__
 constexpr bool kIsRecovery = true;
-#else
-constexpr bool kIsRecovery = false;
-#endif
 
 DeviceInfo::DeviceInfo() {
     std::string scratch_device = android::snapshot::GetScratchOtaMetadataPartition();
